8d429c0c7af69a4d35242db324095c950f95707c
moabdels <mkabdelsater@gmail.com>
2024-07-18 11:52:29 +0300

    Initialize header

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,24 @@
 /*   By: moabdels <moabdels@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 16:28:21 by moabdels          #+#    #+#             */
-/*   Updated: 2024/07/16 16:28:22 by moabdels         ###   ########.fr       */
+/*   Updated: 2024/07/18 11:51:38 by moabdels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+#endif
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_list
+{
+	char			*str_buf;
+	struct s_list	*next;
+}	t_list
 
