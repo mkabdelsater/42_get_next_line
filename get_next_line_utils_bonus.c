bbb07404bf13e771cdf2462f6bb3cb5715729061
moabdels <mkabdelsater@gmail.com>
2024-07-18 15:46:34 +0300

    Add len_to_next_line

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -6,10 +6,31 @@
 /*   By: moabdels <moabdels@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 16:28:24 by moabdels          #+#    #+#             */
-/*   Updated: 2024/07/18 15:36:57 by moabdels         ###   ########.fr       */
+/*   Updated: 2024/07/18 15:46:05 by moabdels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-int	len_to_next_line(t_list *node)
+int	len_to_next_line(t_list *list_node)
+{
+	int	index;
+	int	len;
+
+	if (list_node == NULL)
+		return (0);
+	len = 0;
+	while (list_node)
+	{
+		index = 0;
+		while (list_node->str_buf[index])
+		{
+			if (list_node->str_buf[index] == '\n')
+				return (++len);
+			index++;
+			len++;
+		}
+		list_node = list_node->next;
+	}
+	return (len);
+}
